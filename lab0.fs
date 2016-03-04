613857c65bf2fc3961725b5f289704599a32ca19
Maxim Dankovtsev <max.dankow@gmail.com>
2016-03-04 22:12:39 +0300

    Add lab0.

---
+++ lab0.fs
@@ -0,0 +1,16 @@
+﻿let rec pascal col row =
+    if col=0 || col=row
+    then 1
+    else pascal col (row - 1) + pascal (col - 1) (row - 1)
+
+let printPascalLn n = 
+    for i = 0 to n do printf "%d " (pascal i n)
+
+printPascalLn 4
+
+let printIt n = 
+    for i = 0 to n do 
+        for i = 0 to n do printf "%d " (pascal i n)
+        printfn ""
+                 
+printIt 10
