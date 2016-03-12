01b41fdf672d1b9b6a64a1dadcac88f54c3d0c14
Maxim Dankovtsev <max.dankow@gmail.com>
2016-03-12 14:50:21 +0300

    Add sem3

--- lab0.fs
+++ lab0.fs
@@ -1,11 +1,44 @@
-﻿let rec pascal col row =
+﻿module lab0
+
+open System
+open System.Net
+open System.Collections.Specialized
+
+let (email, name) = ("max.dankow@gmail.com", "Dankovtsev M. A.") // адрес почты и фамилия с инициалами
+
+let rec pascal col row =
     if col=0 || col=row
     then 1
     else pascal col (row - 1) + pascal (col - 1) (row - 1)
 
 let printIt n = 
-    for i = 0 to n do 
-        for j = 0 to i do printf "%d " (pascal j i)
-        printfn ""
-                 
-printIt 20
+  "[" +
+  ([for x in 0..n do for y in 0..x do yield pascal y x] 
+    |> List.map (fun x -> x.ToString())
+    |> List.reduce (fun x y -> x + "," + y) )
+  + "]"
+
+let main () = 
+  let values = new NameValueCollection()
+  values.Add("email", email)
+  values.Add("name", name)
+  values.Add("content", printIt 20)
+
+  let client = new WebClient()
+  let response = client.UploadValues(new Uri("http://91.239.143.158:13666/lab0"), values)
+  let responseString = Text.Encoding.Default.GetString(response)
+
+  printf "%A\n" responseString
+
+
+
+let values = new NameValueCollection()
+values.Add("email", email)
+values.Add("name", name)
+values.Add("content", printIt 20)
+
+let client = new WebClient()
+let response = client.UploadValues(new Uri("http://91.239.143.158:13666/lab0"), values)
+let responseString = Text.Encoding.Default.GetString(response)
+
+printf "%A\n" responseString
---
+++ sem3.fs
@@ -0,0 +1,47 @@
+let rec zip a b = 
+    match (a,b) with
+    | ([], _) | (_, []) -> []
+    | (x::xs, y::ys) -> (x, y)::(zip xs ys)
+
+let rec append l1 l2 =
+    match l1 with
+    | [] -> l2
+    | h::t -> h :: (append t l2)
+
+append [1; 2] [3; 5; 0]
+
+let rec private fac' acc = function
+    | x when x<=1 -> acc
+    | x -> fac' (acc * x) (x-1)
+
+let fac = fac' 1
+
+// (TR) Инвертирует список.
+let rec private reverse' acc = function
+    | [] -> acc
+    | h::t -> reverse' (h::acc) t
+let reverse = reverse' []
+
+// Возвращает список букв строки.
+let explode s = 
+    [for c in s -> c]
+
+// Собирает строку из списка символов.
+let rec implode2 = function
+    | [] -> ""
+    | h::t -> h.ToString() + (implode2 t)
+
+// (TR) Собирает строку из списка символов.
+let rec private implode' acc = function
+    | [] -> acc
+    | h::t -> implode' (acc + h.ToString()) t
+let implode = implode' ""
+
+(explode >> implode) "abacababa"
+
+let deleteLetter c =
+    let rec delete' c = function
+        | [] -> []
+        | h::t when h<>c-> h::(delete' c t)
+        | h::t -> delete' c t
+    explode >> (delete' c) >> implode
